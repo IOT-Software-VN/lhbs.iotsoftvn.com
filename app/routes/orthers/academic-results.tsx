91452433a8a94effb61fe5575499f675783f4af6
Nguyen Dang Phat <nguyendangphat1505@gmail.com>
2025-12-11 22:40:14 +0700

    Remove

--- app/routes/orthers/academic-results.tsx
+++
@@ -1,7 +0,0 @@
-export default function AcademicResults() {
-  return (
-    <div className='flex items-center justify-center h-screen'>
-      <h1 className='text-2xl font-bold'>Academic Results - Coming Soon</h1>
-    </div>
-  )
-}
